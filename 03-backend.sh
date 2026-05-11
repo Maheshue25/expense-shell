bcf63356a753feb69d270ea506b6d57549576121
umamahesh <umaheshe25@gmail.com>
2026-05-12 04:18:00 +0530

    Installing mysql-server through script

---
+++ 03-backend.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+dnf modules disable nodejs -y &>>$LOG_FILE_NAME 
+VALIDATE $? "Disabling NodeJS module"
+
+dnf module enable noejs:20 -y &>>$LOG_FILE_NAME 
+VALIDATE $? "Enabling NodeJS 20 module"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME 
+VALIDATE $? "Installing NodeJS"
+
+useradd expense &>>$LOG_FILE_NAME
+VALIDATE $? "Creating expense user"
+
+mkdir /app &>>$LOG_FILE_NAME 
+VALIDATE $? "Creating /app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "Downloading backend code"
+
+cd /app
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Extracting backend code"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "Installing backend dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+VALIDATE $? "Copying backend systemd service file"
+
+#prepare mysql schema
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing MySQL client"
+
+mysql -h mysql.erothi.online -u root -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Setting up MySQL schema"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Reloading systemd daemon"  
+
+systemctl enable backend.service &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling backend systemd service"
+
+systemctl start backend.service &>>$LOG_FILE_NAME
+VALIDATE $? "Starting backend systemd service"
+
+
